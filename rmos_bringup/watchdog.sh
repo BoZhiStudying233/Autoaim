d9363d3cc5171b512e245c053d5719098f751c46
S-Canister <975813725@qq.com>
2024-07-21 16:39:40 +0800

    fix:修复自启动问题和调试文档网站链接错误

--- rmos_bringup/watchdog.sh
+++ rmos_bringup/watchdog.sh
@@ -1,17 +1,13 @@
 #! /bin/bash
-#gnome-terminal -e 'bash -c "Path to this watchdog.sh" '
+# gnome-terminal -e 'bash -c "/home/nuc/sh/watchdog.sh" '
 
-#echo "nuc" | sudo -S chmod 777 /dev/bus/usb/003/*
-#echo "nuc" | sudo -s chmod 777 /dev/bus/usb/001/*
-#echo "nuc" | sudo -s chmod 777 /dev/bus/usb/004/*
-#echo "nuc" | sudo -s chmod 777 /dev/bus/usb/002/*
-gnome-terminal -e 'bash -c "source /opt/ros/foxy/setup.bash;source /opt/intel/openvino_2024.2.0/setupvars.sh;source $(pwd)/../install/setup.bash;cd $(pwd)/..;ros2 launch rmos_bringup normal_aim.launch.py " '
+echo "nuc" | sudo -S chmod 777 /dev/bus/usb/001/*
+echo "nuc" | sudo -s chmod 777 /dev/bus/usb/002/*
+echo "nuc" | sudo -s chmod 777 /dev/bus/usb/003/*
+echo "nuc" | sudo -s chmod 777 /dev/bus/usb/004/*
+gnome-terminal -e 'bash -c "source /opt/ros/foxy/setup.bash;source /opt/intel/openvino_2022.3.0/setupvars.sh;source $(pwd)/../install/setup.bash;cd $(pwd)/..;ros2 launch rmos_bringup normal_aim.launch.py " '
 
-# sleep 6
-
-# gnome-terminal -e 'bash -c "source /opt/ros/foxy/setup.bash;source /home/nuc12/Desktop/RMOS_1.0-master/install/setup.bash;cd /home/nuc12/Desktop/RMOS_1.0-master;ros2 bag record /image_raw /imu_quaternion " '
-
-sleep 3
+sleep 6
 
 while true
 do
@@ -21,10 +17,10 @@ detector_pid=$(ps -ef | grep "basic_detector" | grep -v grep | awk '{print $2}')
 processer_pid=$(ps -ef | grep "processer" | grep -v grep | awk '{print $2}')
 comm_pid=$(ps -ef | grep "usb_comm" | grep -v grep | awk '{print $2}')
 
-if [ $cam_pid -ne 0 || $detector_pid -ne 0 || $processer_pid -ne 0 || $comm_pid -ne 0]
+if [ $cam_pid -ne 0 ] && [ $detector_pid -ne 0 ] && [ $processer_pid -ne 0 ] && [ $comm_pid -ne 0]
 then                 #如果程序PID在，则程序在运行
         echo `date`
-        echo "cam_pid is exitable!"
+        echo "all_pid is exitable!"
 
 else
 	echo `date`
@@ -53,15 +49,15 @@ else
         {
         	echo "$cam_pid isnot find!"
         }
-        echo "nuc" | sudo -S chmod 777 /dev/bus/usb/003/*
-        echo "nuc" | sudo -s chmod 777 /dev/bus/usb/001/*
-        echo "nuc" | sudo -s chmod 777 /dev/bus/usb/004/*
+        echo "nuc" | sudo -S chmod 777 /dev/bus/usb/001/*
         echo "nuc" | sudo -s chmod 777 /dev/bus/usb/002/*
+        echo "nuc" | sudo -s chmod 777 /dev/bus/usb/003/*
+        echo "nuc" | sudo -s chmod 777 /dev/bus/usb/004/*
 	gnome-terminal -e 'bash -c "source /opt/ros/foxy/setup.bash;source /opt/intel/openvino_2022.3.0/setupvars.sh;source $(pwd)/../install/setup.bash;cd $(pwd)/..;ros2 launch rmos_bringup normal_aim.launch.py " '
 
 fi
 
-sleep 3
+sleep 6
 
 done
 
